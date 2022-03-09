e892604ab6a73241fc6822e579c05b3b2a450409
Fadhil Andriawan <sabarfadhil@gmail.com>
2022-03-09 08:27:43 +0700

    Updates

--- src/components/fetch/reducer.js
+++
@@ -1,8 +0,0 @@
-export const reducer = (state = {user: '', pass: ''}, action) => {
-    if(action.type === 'ADD'){
-        return {
-            ...state,
-            user: 'Fadhil'
-        }
-    }
-}
